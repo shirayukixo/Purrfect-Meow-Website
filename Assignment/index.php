eb670a85413abe9f0711c706ea151050526bfe96
shirayukixo <82701873+shirayukixo@users.noreply.github.com>
2021-09-11 23:33:17 +0800

    Add files via upload

---
+++ Assignment/index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Purrfect Meow</title>
+    <link rel="stylesheet" href="./CSS/main.css">
+</head>
+<body>
+    <header>
+        <div class="logo">
+            <img src="./Images/logo.png" alt="Purrfect Meow Logo" width="20%" height="20%">
+        </div>
+        <nav>
+            <ul>
+                <li>Home</li>
+                <li>Products</li>
+                <li>Cart</li>
+                <li>My Account</li>
+                <li>FAQ</li>
+                <li>About Us</li>
+                <li>Contact Us</li>
+            </ul>
+        </nav>        
+    </header>
+    <main>
+        <div class="notice">
+            We are having promotion now!😻
+        </div>
+        <div>
+            <img src="./Images/main.jpeg" alt="Cat Image" width="100%">
+        </div>
+        <div class="flex-container">
+            <div class="container1">
+                <h1>Start your cat's journey here!</h1>
+                <br>
+                <ol class="list">
+                    <li>Made with only fresh meat that are produced by local farm</li>
+                    <li>Excellent combo for those with food sensitivies and allergies</li>
+                    <li>Air-dried only, no high heat involved</li>
+                </ol>
+                <br>
+                <h2 class="explore">Explore now!</h2>
+                
+            </div>
+            <div class="container2">
+                <img src="./Images/catFood.jpeg" alt="Cat Food" height="70%">
+            </div>
+        </div>
+    </main>    
+</body> 
+</html>
