54934e6a7c07b7939735bcb4ae163406cbd70ae1
shirayukixo <82701873+shirayukixo@users.noreply.github.com>
2021-09-22 23:54:19 +0800

    Add files via upload

---
+++ Assignment/loginRegister.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Purrfect Meow</title>
+    <link type="text/css" rel="stylesheet" href="./CSS/main.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+    <header>
+            <div class="logo">
+                <img src="./Images/logo.png" alt="Purrfect Meow Logo" width="20%" height="20%">
+            </div>
+            <nav>
+                <ul>
+                    <li>Home</li>
+                    <li>Products</li>
+                    <li>Cart</li>
+                    <li>My Account</li>
+                    <li>FAQ</li>
+                    <li>About Us</li>
+                    <li>Contact Us</li>
+                </ul>
+            </nav>        
+        </header> 
+    <main>
+        <div class="notice">
+            We are having promotion now!😻
+        </div>
+        <div class="box1">
+            <div class="box">
+                
+            </div>
+            <div class="box">
+
+            </div>
+        </div>  
+    </main> 
+    <footer>
+        <div class="footer-container">
+            <div class="footer">
+            <img src="./Images/logo.png" alt="Purrfect Meow Logo" width="80%" height="100%">
+            </div>
+            <div class="footer">
+                <h3>KNOW US MORE!</h3>
+                About Us <br>
+                Our Products <br>              
+                
+            </div>
+            <div class="footer">
+                <h3>QUESTIONS?</h3>
+                Frequently Asked Questions <br>
+                Contact Us <br>
+            </div>
+            <div class="footer">
+                <h3>STALK US HERE!</h3>
+                <a href="https://www.facebook.com/elidewove" class="fa fa-facebook" target="_blank"></a>
+                <a href="https://twitter.com/suetyeeee" class="fa fa-twitter" target="_blank"></a>
+                <a href="https://www.instagram.com/suetyeeee" class="fa fa-instagram" target="_blank"></a>                
+            </div>
+        </div>
+    </footer>  
+</body>
+</html>
